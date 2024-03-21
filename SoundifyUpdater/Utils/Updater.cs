9bc8b704eb5c7a7df6251419b5f3c5fe89f17f20
Scrim <105956702+scrim-dev@users.noreply.github.com>
2024-03-21 02:11:39 -0400

    Update Updater.cs

--- SoundifyUpdater/Utils/Updater.cs
+++ SoundifyUpdater/Utils/Updater.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -9,12 +10,27 @@ namespace SoundifyUpdater.Utils
 {
     internal class Updater
     {
+        private static readonly string agent = 
+        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36 OPR/108.0.0.0";
+
+        private static readonly WebClient WC = new WebClient();
         public static void Update()
         {
+            WC.Headers.Add("User-Agent", agent);
             Program.WorkingOn = "Checking for Soundify...";
             if (File.Exists("Soundify.exe"))
             {
+                try { File.Delete("Soundify.exe"); } catch { }
 
+                try
+                {
+                    WC.DownloadFile("serverurl", "Soundify.exe");
+                }
+                catch { }
+            }
+            else
+            {
+                //If not exist do some shit
             }
         }
     }
