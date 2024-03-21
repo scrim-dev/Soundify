0152b2bad1d0d7b0d6f7c5d232259909d6c08708
Scrim <105956702+scrim-dev@users.noreply.github.com>
2024-03-21 11:10:56 -0400

    Js script I need to inject into app later

---
+++ prevent.js
@@ -0,0 +1,16 @@
+document.addEventListener('contextmenu', function(e) {
+    e.preventDefault();
+});
+function handleKeyDown(event) {
+    if (event.keyCode === 123 || (event.ctrlKey && event.shiftKey && event.keyCode === 73)) 
+    {
+        event.preventDefault();
+    }
+}
+document.addEventListener("keydown", handleKeyDown);
+
+window.addEventListener('wheel', function(e) {
+    if (e.ctrlKey) {
+        e.preventDefault();
+    }
+});
